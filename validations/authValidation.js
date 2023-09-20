3b68851b8b7bdd5e8bc16daf521feb67f7e71b43
Sergey Tsybulkin <sereg1ns@yandex.ru>
2023-09-20 20:28:54 +0300

    refactoring: validation

--- validations/authValidation.js
+++ validations/authValidation.js
@@ -2,16 +2,16 @@ const { Joi, celebrate } = require('celebrate');
 
 const signUpValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required().min(6),
     name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    password: Joi.string().required(),
   }),
 });
 
 const signInValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required(),
   }),
 });
 
--- validations/moviesValidation.js
+++ validations/moviesValidation.js
@@ -1,6 +1,8 @@
 const { Joi, celebrate } = require('celebrate');
 
-const config = require('../config');
+// const config = require('../config');
+
+const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/; // Регулярное выражение для URL
 
 const createMovieValidator = celebrate({
   body: Joi.object().keys({
@@ -9,9 +11,9 @@ const createMovieValidator = celebrate({
     duration: Joi.number().required(),
     year: Joi.number().required(),
     description: Joi.string().required(),
-    image: Joi.string().required().regex(config.REGEX),
-    trailerLink: Joi.string().required().regex(config.REGEX),
-    thumbnail: Joi.string().required().regex(config.REGEX),
+    image: Joi.string().required().regex(URL_REGEXP),
+    trailerLink: Joi.string().required().regex(URL_REGEXP),
+    thumbnail: Joi.string().required().regex(URL_REGEXP),
     movieId: Joi.number().required(),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
